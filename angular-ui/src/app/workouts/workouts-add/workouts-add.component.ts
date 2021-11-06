a2926a701cad4301cc8c9596c513772862b91b2e
tpokora <pokora.tomek@gmail.com>
2021-11-06 21:19:25 +0100

    workouts-add component

---
+++ angular-ui/src/app/workouts/workouts-add/workouts-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workouts-add',
+  templateUrl: './workouts-add.component.html',
+  styleUrls: ['./workouts-add.component.css']
+})
+export class WorkoutsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
