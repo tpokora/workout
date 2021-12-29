d0046edd73e05aa0db3b7918c8bcd72e620c932b
tpokora <pokora.tomek@gmail.com>
2021-12-29 12:11:20 +0000

    create ExerciseSet model

--- python-workout/workouts/models.py
+++ python-workout/workouts/models.py
@@ -1,8 +1,34 @@
+from dataclasses import dataclass
+
 from django.db import models
 
 # Create your models here.
 
 
+@dataclass(init=False)
 class Exercise(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "{name: '%s', description: '%s'}" % (self.name, self.description)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+@dataclass(init=False)
+class ExerciseSet(models.Model):
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    exercise = models.OneToOneField(
+        Exercise,
+        on_delete=models.DO_NOTHING,
+        primary_key=True
+    )
+
+    def __str__(self):
+        return "{sets: %s, reps: %s, exercise: %s}" % (self.sets, self.reps, self.exercise.__str__())
+
+    def __repr__(self):
+        return self.__str__()
--- python-workout/workouts/tests.py
+++ python-workout/workouts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
 # Create your tests here.
+from workouts.models import Exercise, ExerciseSet
+
+
+class ExerciseTest(TestCase):
+
+    def test_model(self):
+        # given
+        exercise = Exercise.objects.create(name="Exercise", description="Description")
+        expected_exercise = Exercise(name="Exercise", description="Description")
+
+        # then
+        self.assertEqual(exercise, expected_exercise)
+        self.assertEqual(exercise.__str__(), expected_exercise.__str__())
+        self.assertEqual(exercise.__repr__(), expected_exercise.__repr__())
+        self.assertTrue(exercise == expected_exercise)
+
+
+class ExerciseSetTest(TestCase):
+
+    def test_model(self):
+        # given
+        exercise = Exercise.objects.create(name="Exercise", description="Description")
+        exercise_set = ExerciseSet.objects.create(sets=5, reps=5, exercise=exercise)
+        expected_exercise = Exercise(name="Exercise", description="Description")
+        expected_exercise_set = ExerciseSet(sets=5, reps=5, exercise=expected_exercise)
+
+        # then
+        self.assertEqual(exercise_set, expected_exercise_set)
+        self.assertEqual(exercise_set.__str__(), expected_exercise_set.__str__())
+        self.assertEqual(exercise_set.__repr__(), expected_exercise_set.__repr__())
+        self.assertTrue(exercise_set == expected_exercise_set)
