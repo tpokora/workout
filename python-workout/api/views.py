4422677e67e49eec9af7b64bc5eb46643e21ed61
tpokora <pokora.tomek@gmail.com>
2021-12-30 12:15:18 +0000

    workout detail api

--- python-workout/api/views.py
+++ python-workout/api/views.py
@@ -2,19 +2,21 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 # Create your pages here.
 from workouts.models import Exercise, Workout
-from api.serializers import ExerciseSerializer, WorkoutSerializer
+from api.serializers import ExerciseSerializer, WorkoutSerializer, WorkoutDetailSerializer
 
 
-class WorkoutView(ListAPIView):
+# Workout API
+class WorkoutAPIView(ListAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
 
 
-class WorkoutDetailView(RetrieveAPIView):
+class WorkoutDetailAPIView(RetrieveAPIView):
     queryset = Workout.objects.all()
-    serializer_class = WorkoutSerializer
+    serializer_class = WorkoutDetailSerializer
 
 
-class ExerciseView(ListAPIView):
+# Exercise API
+class ExerciseAPIView(ListAPIView):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
