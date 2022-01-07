7433819f28f2b8a9cc9b019e5543e2f19d947015
tpokora <pokora.tomek@gmail.com>
2022-01-07 19:34:13 +0000

    changes for heroku

--- workout/settings.py
+++ workout/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
@@ -140,3 +141,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
