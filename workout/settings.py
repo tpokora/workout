43def13905301732f653904f9f1d38d621cb697c
tpokora <pokora.tomek@gmail.com>
2022-01-08 10:47:17 +0000

    changes for heroku

--- workout/settings.py
+++ workout/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import django_heroku
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -86,6 +87,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
